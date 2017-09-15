575c52cbb344ef8353c083a169612d4f042949c7
Amanda <amanda.iaria@gmail.com>
2017-09-14 20:33:36 -0400

    Menus, sprites, background images, and bg music, are all set.

---
+++ Assets/scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+	void Awake(){
+		Debug.Log("music player awake" + GetInstanceID());
+	}
+	void Start(){
+		Debug.Log("music player start" + GetInstanceID());
+		if( instance != null ){
+			Destroy(gameObject);
+			print ("Destroyed Music player");
+		}
+		else {
+			instance = this; //this is true?
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}	
+	//Explanation of the music bug
+	//
+}
